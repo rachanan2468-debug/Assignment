6216e8aeab4011f3654648ad3d82deb91ca41173
Rachana Nagaraj <rachanan2468@gmail.com>
2026-03-09 22:33:27 +0530

    Add files via upload

---
+++ MaxSubarraySum.java
@@ -0,0 +1,18 @@
+package assignment3;
+
+	public class MaxSubarraySum {
+	    public static int maxSubarraySum(int[] nums) {
+	        int res = nums[0];
+	        int maxEnding = nums[0];
+	        for (int i = 1; i < nums.length; i++) {
+	            maxEnding = Math.max(maxEnding + nums[i], nums[i]);
+	            res = Math.max(res, maxEnding);
+	        }
+	        return res;
+	    }
+	    public static void main(String[] args) {
+	        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+	        System.out.println(maxSubarraySum(arr));
+	    }
+	}
+	
